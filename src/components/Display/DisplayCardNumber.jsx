94675e6d8b5f10b534af12347905616489e088f8
cesar <cesarcasas0737@gmail.com>
2024-04-22 18:21:38 -0700

    barebones display passing useState props

--- src/components/Display/DisplayCardNumber.jsx
+++ src/components/Display/DisplayCardNumber.jsx
@@ -1,8 +1,14 @@
 export default function DisplayCardNumber({number}){
 
+    const cardNumSegments = [number]
+    const cardNumString = cardNumSegments.toString();
+    
     return(
-        <div>
-            <h1>{number}</h1>       
+        <div className="flex flex-row gap-3">
+            <h1>{cardNumString.slice(0,4)}</h1>
+            <h1>{cardNumString.slice(4,8)}</h1> 
+            <h1>{cardNumString.slice(8,12)}</h1> 
+            <h1>{cardNumString.slice(12,16)}</h1>      
         </div>
     )
 }
