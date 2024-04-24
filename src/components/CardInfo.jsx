221c4d6ee55c2d5cb294625441679f90277bfb8d
cesar <cesarcasas0737@gmail.com>
2024-04-24 16:48:49 -0700

    setted up Display and Edit components

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react"
+import EditCardName from "./Edit/EditCardName"
+import EditCardNumber from "./Edit/EditCardNumber"
+import EditCardExpiration from "./Edit/EditCardExpiration"
+import EditCardCvv from "./Edit/EditCardCvv"
+
+import DisplayCardName from "./Display/DisplayCardName"
+import DisplayCardExpiration from "./Display/DisplayCardExpiration"
+import DisplayCardCvv from "./Display/DisplayCardCvv"
+import DisplayCardNumber from "./Display/DisplayCardNumber"
+
+
 
 export default function CardInfo(){
     const [creditCard, setCreditCard] = useState({
-        number: '000000000000',
+        number: 1234_5678_9012_3456,
         name: 'James Smith',
         expirationDate: {
             month: '09',
@@ -13,7 +24,52 @@ export default function CardInfo(){
 
     return(
         <>
+        <EditCardName
+            creditCard={creditCard}
+            setCreditCard={setCreditCard}
+        />
+
+        <EditCardNumber
+            creditCard={creditCard}
+            setCreditCard={setCreditCard}
+        />
+
+        <EditCardExpiration
+            creditCard={creditCard}
+            setCreditCard={setCreditCard}
+        />
+
+        <EditCardCvv 
+            creditCard={creditCard}
+            setCreditCard={setCreditCard}
+        />
+
+        <div className=" sm:bg-slate-600">
+            <div className=" bg-blue-300 bg-[url('src/assets/bg-card-front.png')] bg-no-repeat ">
+                <DisplayCardNumber 
+                number={creditCard.number}
+                />
+                <DisplayCardName
+                    name={creditCard.name}
+                />
+                <DisplayCardExpiration
+                    expirationDate={creditCard.expirationDate}
+                />
+            </div>
+
+            <div className=" bg-red-200 bg-[url('src/assets/bg-card-back.png')] bg-no-repeat">
+                <DisplayCardCvv 
+                    cvv={creditCard.cvv}
+                />
+            </div>
+        </div>
+
+   
+       
+
         
+            
+
         </>
     )
 }
